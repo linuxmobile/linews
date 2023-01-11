900b5f1f6a76a251e16b3cb5b7565508bcb07e40
linuxmobile <bdiez19@gmail.com>
2023-01-11 19:13:52 -0300

    Feat 💎: Improved notify work

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,9 +1,20 @@
 const NOTIFYBTN = document.querySelector('#notifyIcon')
 const NOTIFYSECTION = document.querySelector('#notifySection')
+const DELETENOTIFY = document.querySelector('#deleteNotify')
+const NOTIFYELEMENT = document.querySelector('#notifyElement')
+
+let deletedElements = []
 
 export const ntfBtnHandler = () => {
 	NOTIFYBTN.addEventListener('click', () => {
 		NOTIFYSECTION.classList.toggle('hidden')
 		NOTIFYSECTION.classList.toggle('flex')
+	})
+	DELETENOTIFY.addEventListener('click', () => {
+		const parent = DELETENOTIFY.parentNode
+        const elementHTML = parent.innerHTML
+		deletedElements.push(elementHTML)
+		parent.remove()
+		localStorage.setItem('deletedElements', JSON.stringify(deletedElements))
 	})
 }
