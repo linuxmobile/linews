a7a0f9a38f452dc59bc67f857a0538ee96fcf60f
linuxmobile <bdiez19@gmail.com>
2023-01-12 16:32:09 -0300

    Style 💅🏻: added hover and selected style

--- src/services/filter.js
+++ src/services/filter.js
@@ -1,16 +1,36 @@
 import { getArticles, POSTSECTION, renderPostsSection, tag, updateTag } from '@services/articles'
 
-const categoryButtons = document.querySelectorAll('[data-category]');
+const categoryButtons = document.querySelectorAll('[data-category]')
 
-categoryButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const category = button.dataset.category;
-        if (category === 'popular') {
-            updateTag(null);
-        } else {
-            updateTag(category);
-        }
-        POSTSECTION.innerHTML = '';
-        getArticles().then(posts => renderPostsSection(posts));
-    });
-});
+const categoryStyles = {
+	'popular': ['bg-pink-800/10', 'text-pink-800', 'border-[1px]', 'border-pink-800'],
+	'react': ['bg-cyan-300/10', 'text-cyan-300/70', 'border-[1px]', 'border-cyan-300/70'],
+	'linux': ['bg-violet-400/10', 'text-violet-400/50', 'border-[1px]', 'border-violet-400/50'],
+	'github': ['bg-green-700/10', 'text-green-700', 'border-[1px]', 'border-green-700'],
+	'opensource': ['bg-red-500/10', 'text-red-500', 'border-[1px]', 'border-red-500'],
+	'javascript': ['bg-yellow-500/10', 'text-yellow-500/75', 'border-[1px]', 'border-yellow-500/75']
+}
+
+categoryButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const category = button.dataset.category
+		if (category === 'popular') {
+			updateTag(null)
+		} else {
+			updateTag(category)
+		}
+		POSTSECTION.innerHTML = ''
+		getArticles().then((posts) => renderPostsSection(posts))
+
+		categoryButtons.forEach(btn => {
+            Object.keys(categoryStyles).forEach(category => {
+                categoryStyles[category].forEach(className => {
+                    btn.classList.remove(className)
+                })
+            })
+        })
+        categoryStyles[category].forEach(className => {
+            button.classList.add(className)
+        })
+	})
+})
