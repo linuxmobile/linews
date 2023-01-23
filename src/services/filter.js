cd5abbfab56b9ebd7ceed9b37057155e1e50c889
linuxmobile <bdiez19@gmail.com>
2023-01-23 19:13:41 -0300

    Style 🦄: changing styles

--- src/services/filter.js
+++ src/services/filter.js
@@ -3,12 +3,12 @@ import { getArticles, POSTSECTION, renderPostsSection, tag, updateTag } from '@s
 const categoryButtons = document.querySelectorAll('[data-category]')
 
 const categoryStyles = {
-	'popular': ['bg-pink-800/10', 'text-pink-800', 'border-[1px]', 'border-pink-800'],
-	'react': ['bg-cyan-300/10', 'text-cyan-300/70', 'border-[1px]', 'border-cyan-300/70'],
-	'linux': ['bg-violet-400/10', 'text-violet-400/50', 'border-[1px]', 'border-violet-400/50'],
-	'github': ['bg-green-700/10', 'text-green-700', 'border-[1px]', 'border-green-700'],
-	'opensource': ['bg-red-500/10', 'text-red-500', 'border-[1px]', 'border-red-500'],
-	'javascript': ['bg-yellow-500/10', 'text-yellow-500/75', 'border-[1px]', 'border-yellow-500/75']
+	popular: ['bg-pink-800/20', 'text-pink-800', 'border-[1px]', 'border-pink-800'],
+	react: ['bg-cyan-300/20', 'text-cyan-300/70', 'border-[1px]', 'border-cyan-300/70'],
+	linux: ['bg-violet-400/20', 'text-violet-400/50', 'border-[1px]', 'border-violet-400/50'],
+	github: ['bg-green-700/20', 'text-green-700', 'border-[1px]', 'border-green-700'],
+	opensource: ['bg-red-500/20', 'text-red-500', 'border-[1px]', 'border-red-500'],
+	javascript: ['bg-yellow-500/20', 'text-yellow-500/75', 'border-[1px]', 'border-yellow-500/75']
 }
 
 categoryButtons.forEach((button) => {
@@ -22,15 +22,15 @@ categoryButtons.forEach((button) => {
 		POSTSECTION.innerHTML = ''
 		getArticles().then((posts) => renderPostsSection(posts))
 
-		categoryButtons.forEach(btn => {
-            Object.keys(categoryStyles).forEach(category => {
-                categoryStyles[category].forEach(className => {
-                    btn.classList.remove(className)
-                })
-            })
-        })
-        categoryStyles[category].forEach(className => {
-            button.classList.add(className)
-        })
+		categoryButtons.forEach((btn) => {
+			Object.keys(categoryStyles).forEach((category) => {
+				categoryStyles[category].forEach((className) => {
+					btn.classList.remove(className)
+				})
+			})
+		})
+		categoryStyles[category].forEach((className) => {
+			button.classList.add(className)
+		})
 	})
 })
